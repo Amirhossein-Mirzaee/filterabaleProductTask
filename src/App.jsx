5f61b46565342ba2b065175445b91fa921b22df8
Amirhossein Mirzaee <amir2021zx@gmail.com>
2023-12-10 23:23:54 -0800

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,49 +5,51 @@ import SortAndFilter from "./components/SortAndFilter";
 import { useEffect, useState } from "react";
 
 const App = () => {
-  const [data, setData] = useState([]);
-  const [newData, setNewData] = useState(...data);
+  const [database, setDatabase] = useState([]); // temp
+  const [uiData, setUiData] = useState([]);
   const [startPrice, setStartPrice] = useState(0);
   const [endPrice, setEndPrice] = useState(0);
 
   useEffect(() => {
-    const fetchData = async () => {
+    (async () => {
       try {
         const response = await fetch("https://api.escuelajs.co/api/v1/products");
         const result = await response.json();
-        setData(result.slice(0, 20));
-
+        const myProducts = result.slice(0, 20);
+        setDatabase(myProducts);
+        setUiData(myProducts);
       } catch (err) {
         console.error(err);
       }
-    };
-    fetchData();
+    })();
   }, []);
 
   const handleIncrese = () => {
-    const sortedData = [...data].sort((a, b) => a.price - b.price);
-    setData(sortedData);
+    const sortedData = [...database].sort((a, b) => a.price - b.price);
+    setUiData(sortedData);
   };
   const hanldeDecrese = () => {
-    const sortedData = [...data].sort((a, b) => b.price - a.price);
-    setData(sortedData);
+    const sortedData = [...database].sort((a, b) => b.price - a.price);
+    setUiData(sortedData);
   };
+
   const handleFirst = (e) => {
     setStartPrice(e.target.value);
-    console.log(startPrice);
+    // console.log(startPrice);
   };
+
   const handleSec = (e) => {
     setEndPrice(e.target.value);
     console.log(endPrice);
   };
+
   const formHanlde = (e) => {
     e.preventDefault();
     if (endPrice && startPrice) {
-      const filteredData = data.filter(
+      const filteredData = database.filter(
         (item) => +item.price >= +startPrice && +item.price <= +endPrice
       );
-      setNewData(filteredData);
-      console.log(newData);
+      setUiData(filteredData);
     }
   };
   return (
@@ -59,7 +61,7 @@ const App = () => {
         handleSec={handleSec}
         formHandle={formHanlde}
       />
-      <Prdoucts data={newData ? newData : data} />
+      <Prdoucts uiData={uiData} />
     </div>
   );
 };
--- src/components/Prdoucts.jsx
+++ src/components/Prdoucts.jsx
@@ -1,20 +1,33 @@
 import { LazyLoadImage } from "react-lazy-load-image-component";
-import "../styles/products.css"
-const Prdoucts = ({ data }) => {
+import "../styles/products.css";
+const Prdoucts = ({ uiData }) => {
   return (
-    <section className="conatiner">
+    <section className="conatiner overflow-hidden">
       <div className="row">
-        {data.map((item) => (
-          <div className="col-md-5 col-lg-4 mx-auto align-items-center  p-sm-5 " key={item.id}>
+        {uiData.map((item) => (
+          <div
+            className="col-md-5 col-lg-4 mx-auto align-items-center  p-md-2 "
+            key={item.id}
+          >
             <div className="card col p-sm-4 overflow-hidden ">
               <div
                 className="d-flex align-items-center justify-content-center col "
                 style={{ height: "150px" }}
               >
-                <LazyLoadImage className="card-img" src={item.images[2]} alt={item.id} style={{ maxWidth: "470px" }} />
+                <LazyLoadImage
+                  className="card-img"
+                  src={item.images[2]}
+                  alt={item.id}
+                  style={{ maxWidth: "470px" }}
+                />
               </div>
               <div className="card-body text-center h3 ">
-                <p id="title" className="card-title h2 text-nowrap d-flex justify-content-center p-2">{item.title}</p>
+                <p
+                  id="title"
+                  className="card-title h2 text-nowrap d-flex justify-content-center p-2"
+                >
+                  {item.title}
+                </p>
                 <p className="card-text">{item.price} $</p>
               </div>
             </div>
